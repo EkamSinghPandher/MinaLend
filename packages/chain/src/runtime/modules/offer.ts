ceaaa92ef1bc379c10056c11947762be9fc14b6b
EkamSinghPandher <ekamsingh@Ekam-Singhs-MacBook-Pro.local>
2024-09-21 15:28:33 +0800

    add offer struct

---
+++ packages/chain/src/runtime/modules/offer.ts
@@ -0,0 +1,41 @@
+import { PublicKey } from "o1js";
+
+class Offer{
+    offerId: string;
+    lender: PublicKey;                 // The lender's identifier
+    borrower: PublicKey;                  // The borrower's identifier
+    annualInterestRate: number;      // The annual interest rate (as a percentage or decimal)
+    token: string;                     // The token/currency of the loan
+    amount: number;                    // The lending amount offered
+    period: number;                    // The lending period in days
+    minPropertyValue: number;        // The minimum property value that has to be proved
+    minIncomeMonthly: number;        // The minimum monthly income that has to be proved
+    penalty: number;                   // The annual penalty interest rate (as a percentage or decimal)
+    status: number;               // The current status of the offer
+
+    constructor(
+        offerId: string,
+        lender: PublicKey,
+        borrower: PublicKey,
+        annualInterestRate: number,
+        token: string,
+        amount: number,
+        period: number,
+        minPropertyValue: number,
+        minIncomeMonthly: number,
+        penalty: number,
+        status: number
+    ) {
+        this.offerId = offerId;
+        this.lender = lender;
+        this.borrower = borrower;
+        this.annualInterestRate = annualInterestRate;
+        this.token = token;
+        this.amount = amount;
+        this.period = period;
+        this.minPropertyValue = minPropertyValue;
+        this.minIncomeMonthly = minIncomeMonthly;
+        this.penalty = penalty;
+        this.status = status;
+    }
+}
